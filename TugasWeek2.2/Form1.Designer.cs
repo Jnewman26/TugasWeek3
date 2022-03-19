17da5567622e9d40904da217ed7915e0b3a4d309
James Newman <James Newman@DESKTOP-RIVU26V>
2022-03-19 15:32:50 +0700

    Add project files.

---
+++ TugasWeek2.2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TugasWeek2._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonProses = new System.Windows.Forms.Button();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Data:";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(32, 70);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(482, 31);
+            this.textBoxInput.TabIndex = 1;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // buttonProses
+            // 
+            this.buttonProses.Location = new System.Drawing.Point(32, 135);
+            this.buttonProses.Name = "buttonProses";
+            this.buttonProses.Size = new System.Drawing.Size(121, 64);
+            this.buttonProses.TabIndex = 2;
+            this.buttonProses.Text = "Proses";
+            this.buttonProses.UseVisualStyleBackColor = true;
+            this.buttonProses.Click += new System.EventHandler(this.buttonProses_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOutput.Location = new System.Drawing.Point(25, 239);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(167, 42);
+            this.labelOutput.TabIndex = 3;
+            this.labelOutput.Text = "[EMPTY]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.buttonProses);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form Pengaturan Warna";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonProses;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
+
---
+++ TugasWeek2.2/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TugasWeek2._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonProses_Click(object sender, EventArgs e)
+        {
+            if (textBoxInput.Text.ToLower() == "hide") // cek text input apakah isi nya hide
+            {
+                labelOutput.Hide();
+            }
+            else if (textBoxInput.Text.ToLower() == "shown") // cek text input apakah isinya shown
+            {
+                labelOutput.Show();
+            }
+            else
+            {
+                string hapus = "isi:";
+                string warna = "warna:";
+                string biru = "biru";
+                string merah = "merah";
+                string hijau = "hijau";
+                string hitam = "default";
+                string restart = "[EMPTY]";
+                if (textBoxInput.Text.ToLower().Contains(hapus)) // cek apakah mengandung isi:
+                {
+                    labelOutput.Text = textBoxInput.Text.Remove(textBoxInput.Text.ToLower().IndexOf(hapus), hapus.Length);
+                    // ini label yg mau diganti = input.text
+                }
+                else if (textBoxInput.Text.ToLower().Contains(warna))
+                {
+                    string color = textBoxInput.Text.Remove(textBoxInput.Text.IndexOf(warna), warna.Length);
+                    if (color.ToLower().Contains(biru))
+                    {
+                        labelOutput.ForeColor = Color.Blue; // ini buat ganti warna
+                    }
+                    if (color.ToLower().Contains(merah))
+                    {
+                        labelOutput.ForeColor = Color.Red; // ini buat ganti warna
+                    }
+                    if (color.ToLower().Contains(hijau))
+                    {
+                        labelOutput.ForeColor = Color.Green; // ini buat ganti warna
+                    }
+                    if (color.ToLower().Contains(hitam))
+                    {
+                        labelOutput.ForeColor = Color.Black; // ini buat ganti warna
+                    }
+                }
+                else if (textBoxInput.Text == "besarkan")
+                {
+                    labelOutput.Font = new Font("Microsoft San Serif", labelOutput.Font.Size + 1);
+                }
+                else if (textBoxInput.Text == "kecilkan")
+                {
+                    labelOutput.Font = new Font("microsoft san serif", labelOutput.Font.Size - 1);
+                }
+                else if (textBoxInput.Text.ToLower() == "restart")
+                {
+                    labelOutput.Text = restart;
+                    labelOutput.ForeColor = Color.Black;
+                    labelOutput.Font = new Font("microsoft san serif", 14);
+                }
+                else
+                {
+                    MessageBox.Show("Syntax tidak ditemukan");
+                }
+            }
+        }
+    }
+}
